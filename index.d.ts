2fda6bea3c752814c30a5fc8688d3b1eeb04f2a6
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-02-20 11:13:26 +0100

    fixed typings

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,26 @@
-declare module 'swe-validation' {
-
-    interface cin {
-        isValid:boolean,
-        corporation:{
-            type:string,
-            id:string
-        }
-    }
-
-    interface ssn {
-        isValid:boolean,
-        person?: {
-            type:string,
-            sex:string,
-            ssn:string
-        }
-    }
-
-    let validate: {
-        ssn(number:number): ssn;
-        cin(number:number): cin;
+// Type definitions for swe-validation 1.0
+// Project: https://github.com/keype/swe-validation
+// Definitions by: Robert Hjalmers <https://github.com/hjalmers>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+interface cin {
+    isValid: boolean;
+    corporation: {
+        type: string;
+        id: string;
     };
-    export = validate;
 }
+
+interface ssn {
+    isValid: boolean;
+    person?: {
+        type: string;
+        sex: string;
+        ssn: string;
+    };
+}
+
+declare let validate: {
+    ssn(number: number): ssn;
+    cin(number: number): cin;
+};
+export = validate;
